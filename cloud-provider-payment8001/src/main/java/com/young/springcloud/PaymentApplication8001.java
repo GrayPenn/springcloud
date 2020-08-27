4d2aac995392789ab633551077996f500f166a63
Young <421813163@qq.com>
2020-08-27 20:33:03 +0800

    20200827: 1、08支付模块构建（上）；

---
+++ cloud-provider-payment8001/src/main/java/com/young/springcloud/PaymentApplication8001.java
@@ -0,0 +1,20 @@
+package com.young.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pgy
+ * Date: 2020/8/27
+ * Time: 8:28 下午
+ **/
+@SpringBootApplication
+public class PaymentApplication8001 {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8001.class,args);
+    }
+
+}
