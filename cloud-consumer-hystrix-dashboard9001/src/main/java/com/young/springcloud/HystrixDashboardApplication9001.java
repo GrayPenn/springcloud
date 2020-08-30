09f955c5ab60047ce4090f341a0f0217dc1b49c2
Young <421813163@qq.com>
2020-08-30 21:59:44 +0800

    20200830:64.Hystrix图形化 Dashboard 监控实战;

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/young/springcloud/HystrixDashboardApplication9001.java
@@ -0,0 +1,19 @@
+package com.young.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @date: 2020/8/30 9:48 下午
+ * @author: pgy
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication9001.class, args);
+    }
+}
